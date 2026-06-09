527f1e8ec5b11c427b537131e01542f4d925cb0b
Rosatto Constanza <rosattoconstanznstanza@gmail.com>
2026-06-08 22:46:24 -0300

    "Avanzando con lo principal"

--- src/main/java/org/modelo/logica/Scannear.java
+++ src/main/java/org/modelo/logica/Scannear.java
@@ -6,11 +6,12 @@ public class Scannear {
 
     //------------------------------------------------------------
     static Mundial mundial;
+
     public static void inicializarMundial() {
         Scanner sc = new Scanner(System.in);
         boolean excepcion = false;
 
-        while(excepcion == false) {
+        while (excepcion == false) {
             try {
                 System.out.println("Para comenzar, necesitamos datos iniciales: ");
                 System.out.println("Ingrese Año: ");
@@ -26,8 +27,7 @@ public class Scannear {
                 mundial = new Mundial(anio, mascota, fechaInicio, fechaFinal);
                 System.out.println("El Mundial se creo correctamente, muchas gracias.");
                 excepcion = true;
-            }
-            catch (InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Lamentamos la interrupcion, parece que se ingresó un " +
                         "texto que deberia ser un entero, intente nuevamente.");
                 sc.nextLine();
@@ -41,24 +41,28 @@ public class Scannear {
         Scanner sc = new Scanner(System.in);
         boolean salir = false;
 
-        while(salir == false) {
+        while (salir == false) {
             try {
                 System.out.println("¡Bienvenido a la organización del Mundial de fútbol 2026! ¿Que desea hacer?:\n" +
                         "1. Gestión de Infraestructura.\n" + "2. Administración de Delegaciones.\n" +
                         "3.Organización Deportiva.\n" + "4.Registro de Eventos de Campo.\n" + "5.Informes.\n" + "6. Salir.");
                 int eleccionUsuario = numValido(sc, 1, 6);
                 switch (eleccionUsuario) {
-                    case 1: GestionDeInfraestructura(); break;
-                    case 2: AdministracionDeDelegaciones(); break;
+                    case 1:
+                        GestionDeInfraestructura();
+                        break;
+                    case 2:
+                        AdministracionDeDelegaciones();
+                        break;
                     case 3:
                     case 4:
                     case 5:
-                    case 6: System.out.println("Gracias por utilizar nuestro sistema, vuelva pronto :)");
+                    case 6:
+                        System.out.println("Gracias por utilizar nuestro sistema, vuelva pronto :)");
                         salir = true;
                         break;
                 }
-            }
-            catch(InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Lamentamos la interrupción, parece que se no ingresó un entero, intente nuevamente.");
                 sc.nextLine();
             }
@@ -90,10 +94,10 @@ public class Scannear {
         }
     }
 
-    public static void registroDeEventosDeCampo () {
+    public static void registroDeEventosDeCampo() {
     }
 
-    public static void configurarGrupo () {
+    public static void configurarGrupo() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Cantidad de grupos a añadir: ");
         int cantidad = sc.nextInt();
@@ -106,19 +110,24 @@ public class Scannear {
     public static void GestionDeInfraestructura() {
         Scanner sc = new Scanner(System.in);
         boolean volver = false;
-        while(volver == false) {
+        while (volver == false) {
             try {
-                System.out.println("¿En que nos enfocamos?\n" +"1. Registrar Sede. \n" +
+                System.out.println("¿En que nos enfocamos?\n" + "1. Registrar Sede. \n" +
                         "2. Registrar Estadio a Sede. \n" + "3. Volver.");
                 int eleccionusuario = numValido(sc, 1, 3);
 
-                switch(eleccionusuario) {
-                    case 1: registrarSede(); break;
-                    case 2: registrarEstadio(); break;
-                    case 3: volver = true; break;
+                switch (eleccionusuario) {
+                    case 1:
+                        registrarSede();
+                        break;
+                    case 2:
+                        registrarEstadio();
+                        break;
+                    case 3:
+                        volver = true;
+                        break;
                 }
-            }
-            catch(InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Lamentamos la interrupción, parece que se no ingresó un entero, intente nuevamente.");
                 sc.nextLine();
             }
@@ -129,7 +138,7 @@ public class Scannear {
         Scanner sc = new Scanner(System.in);
         boolean excepcion = false;
 
-        while(excepcion == false) {
+        while (excepcion == false) {
             try {
                 System.out.println("Ingrese la ciudad de la Sede: ");
                 String ciudad = sc.nextLine();
@@ -148,8 +157,7 @@ public class Scannear {
                 mundial.agregarSede(sede);
                 System.out.println("La Sede se creó correctamente.");
                 excepcion = true;
-            }
-            catch(InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Lamentamos la interrupción, parece que se no ingresó un número, intente nuevamente.");
                 sc.nextLine();
             }
@@ -161,12 +169,12 @@ public class Scannear {
         boolean excepcion = false;
         List<Sede> sedes = mundial.getSedes();
 
-        if(sedes.isEmpty()) {
+        if (sedes.isEmpty()) {
             System.out.println("Parece que aún no se registraron sedes, vuelva a intentarlo.");
             return;
         }
         listarSedes();
-        while(excepcion == false) {
+        while (excepcion == false) {
             try {
                 System.out.println("Ingrese el número de Sede que le desea añadir un Estadio: ");
                 int eleccionUsuario = numValido(sc, 1, sedes.size());
@@ -183,8 +191,7 @@ public class Scannear {
                 sede.agregarEstadio(e);
                 System.out.println("El Estadio se creó correctamente.");
                 excepcion = true;
-            }
-            catch(InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Lamentamos la interrupción, parece que se no ingresó un número, intente nuevamente.");
                 sc.nextLine();
             }
@@ -197,22 +204,26 @@ public class Scannear {
         Scanner sc = new Scanner(System.in);
         boolean volver = false;
 
-        while(volver == false) {
+        while (volver == false) {
             try {
                 System.out.println("¿En que nos enfocamos?\n" + "1. Cargar Pais participante.\n" +
                         "2. Cargar Seleccion y su lista de jugadores.\n" + "3. Cargar Cuerpo Tecnico y Director Tecnico.\n" +
-                        "4. Volver." );
+                        "4. Volver.");
                 int eleccionUsuario = numValido(sc, 1, 4);
 
                 switch (eleccionUsuario) {
-                    case 1: cargarPais(); break;
-                    case 2: cargarSeleccion(); break;
+                    case 1:
+                        cargarPais();
+                        break;
+                    case 2:
+                        cargarSeleccionyJugadores();
+                        break;
                     case 3:
-                    case 4: volver = true;
+                    case 4:
+                        volver = true;
                         break;
                 }
-            }
-            catch(InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Lamentamos la interrupción, parece que se no ingresó un número, intente nuevamente.");
                 sc.nextLine();
             }
@@ -231,13 +242,13 @@ public class Scannear {
 
         Pais pais = new Pais(nombrePais, banderaPais);
         System.out.println("El pais se creó correctamente, vamos a asignarlo a una Sede.");
-        if(sedes.isEmpty()) {
+        if (sedes.isEmpty()) {
             System.out.println("Parece que aún no se registraron sedes, vuelva a intentarlo.");
             return;
         }
         listarSedes();
 
-        while(excepcion == false) {
+        while (excepcion == false) {
             try {
                 System.out.println("Ingrese el número de Sede que le desea asignar al Pais: ");
                 int eleccionUsuario = numValido(sc, 1, sedes.size());
@@ -249,8 +260,7 @@ public class Scannear {
 
                 System.out.println("Muchas gracias, la Sede ya tiene un Pais asignado.");
                 excepcion = true;
-            }
-            catch(InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Lamentamos la interrupción, parece que se no ingresó un número, intente nuevamente.");
                 sc.nextLine();
             }
@@ -258,7 +268,7 @@ public class Scannear {
     }
 
 
-    private static void cargarSeleccion() {
+    private static void cargarSeleccionyJugadores() {
         Scanner sc = new Scanner(System.in);
 
         if (paises.isEmpty()) {
@@ -297,14 +307,50 @@ public class Scannear {
             pais.setSeleccion(seleccion);
             System.out.println("La seleccion se creó correctamente. Continuemos con sus jugadores...");
 
-            //cargarJugadores(); //Lo separo porque si no queda mucha informacion junta en un solo metodo
 
+            System.out.println("¿Cuantos jugadores desea añadir?: ");
+            int cantidadJugadores = sc.nextInt();
+            sc.nextLine();
+
+            for (int i = 0; i < cantidadJugadores; i++) {
+                System.out.println("Ingrese el nombre del jugador " + (i + 1) + ": ");
+                String nombre = sc.nextLine();
+                System.out.println("Ingrese la fecha de nacimiento del jugador " + (i + 1) + ": ");
+                int nacimiento = sc.nextInt();
+                System.out.println("Ingrese el número del jugador " + (i + 1) + ": ");
+                int dorsal = sc.nextInt();
+                System.out.println("Ingrese el peso del jugador " + (i + 1) + ": ");
+                float peso = sc.nextFloat();
+                System.out.println("Ingrese la altura del jugador " + (i + 1) + ": ");
+                float altura = sc.nextFloat();
+                System.out.println("Ingrese la posición del jugador " + (i + 1) +
+                        " (¡Verifique escribir una opción correctamente! -> Arquero, Defensor, Mediocampista, Delantero): ");
+                String posicionJug = sc.nextLine();
+
+                Posicion posicion = null;
+                boolean posicionValida = false;
+
+                while (!posicionValida) {
+                    try {
+                        posicion = Posicion.valueOf(posicionJug);
+                        posicionValida = true;
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Parece que la posición ingresada no es válida, intente nuevamente: ");
+                        posicionJug = sc.nextLine();
+                    }
+                }
+
+                Jugador jugador = new Jugador(nombre, nacimiento, dorsal, posicion, peso, altura);
+                seleccion.agregarJugador(jugador);
+                System.out.println("Jugador " + (i+1) + " agregado correctamente.");
+            }
         }
-        catch(InputMismatchException e) {
+        catch (InputMismatchException e) {
             System.out.println("Lamentamos la interrupción, parece que se ingresó un dato que no corresponde, intente nuevamente.");
             sc.nextLine();
         }
     }
+//no se corta la linea esta ;( aaaaaa
 
 
 
@@ -320,57 +366,45 @@ public class Scannear {
 
 
 
+        //---------------------------------------------------------------------------
+        private static int numValido (Scanner sc,int min, int max){
+            int op;
 
+            do {
+                System.out.print("Ingrese una opción válida: ");
+                op = sc.nextInt();
 
+                if (op < min || op > max) {
+                    System.out.println("Opción inválida.");
+                }
 
+            } while (op < min || op > max);
 
+            return op;
+        }
 
-
-
-
-
-    //---------------------------------------------------------------------------
-    private static int numValido(Scanner sc, int min, int max) {
-        int op;
-
-        do {
-            System.out.print("Ingrese una opción válida: ");
-            op = sc.nextInt();
-
-            if (op < min || op > max) {
-                System.out.println("Opción inválida.");
-            }
-
-        } while (op < min || op > max);
-
-        return op;
-    }
-
-    private static void listarSedes() {
-        System.out.println("Sedes registradas:\n");
-        int i = 1;
-        for (Sede s : mundial.getSedes()) {
-            System.out.println(i++ + ". " + s);
-            for (Estadio e : s.getEstadios()) {
-                System.out.println("Estadio -> " + e);
+        private static void listarSedes () {
+            System.out.println("Sedes registradas:\n");
+            int i = 1;
+            for (Sede s : mundial.getSedes()) {
+                System.out.println(i++ + ". " + s);
+                for (Estadio e : s.getEstadios()) {
+                    System.out.println("Estadio -> " + e);
+                }
             }
         }
-    }
 
-    private static void listarPaises() {
-        System.out.println("Países registrados:\n");
-        for (int i = 0; i < paises.size(); i++) {
-            Pais p = paises.get(i);
-            String sel = "Sin selección";
-            if(p.getSeleccion() != null) {
-                sel = p.getSeleccion().getNombreFederacion();
+        private static void listarPaises () {
+            System.out.println("Países registrados:\n");
+            for (int i = 0; i < paises.size(); i++) {
+                Pais p = paises.get(i);
+                String sel = "Sin selección";
+                if (p.getSeleccion() != null) {
+                    sel = p.getSeleccion().getNombreFederacion();
+                }
+                System.out.println((i + 1) + ". " + p + " | Seleccion -> " + sel);
             }
-            System.out.println((i + 1) + ". " + p + " | Seleccion -> " + sel);
         }
-    }
 }
 
-//private static void cargarJugadores(){
-    //Scanner sc = new Scanner(System.in);
-    //System.out.println("¿Cuantos jugadores desea añadir?: ");
-//}
+
