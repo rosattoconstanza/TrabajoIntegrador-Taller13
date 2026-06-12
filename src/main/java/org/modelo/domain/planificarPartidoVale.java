a0ea8d75e2c0251f13bd0d09f5938caa97d8b81f
Vale Rougier <valeroufacultad@gmail.com>
2026-06-12 19:45:16 -0300

    metodo nuevo

--- src/main/java/org/modelo/domain/planificarPartidoVale.java
+++ src/main/java/org/modelo/domain/planificarPartidoVale.java
@@ -1,11 +1,20 @@
 package org.modelo.domain;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class planificarPartidoVale {
     public Partido planificarPartido(ArrayList<Fase> fases, ArrayList<Grupo> grupos) {
         Scanner sc = new Scanner(System.in);
+        for (int i=0; i<fases.size(); i++){
+            System.out.println(i + "-" + fases.get(i));
+        }
+        System.out.println("En que fase se encuentra el partido que cargara?");
+        int respuesta = sc.nextInt();
+        Fase fase = fases.get(respuesta);
+        
+
+
+
 
 
 
