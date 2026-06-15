359a04ea714f1643f2c16e8d1668ae09d88b6254
Vale Rougier <valeroufacultad@gmail.com>
2026-06-15 13:01:16 -0300

    Planificar Partido

--- src/main/java/org/modelo/domain/planificarPartidoVale.java
+++ src/main/java/org/modelo/domain/planificarPartidoVale.java
@@ -1,6 +1,8 @@
 package org.modelo.domain;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -11,7 +13,7 @@ public class planificarPartidoVale {
 
 
 
-    public Partido planificarPartido(ArrayList<Fase> fases, ArrayList<Grupo> grupos) {
+    public Partido planificarPartido(ArrayList<Fase> fases, ArrayList<Grupo> grupos, ArrayList<Pais> paises) {
         Scanner sc = new Scanner(System.in);
         Partido partido = null;
         boolean excepcion = false;
@@ -47,16 +49,81 @@ public class planificarPartidoVale {
             System.out.println("Seleccione selección visitante");
             String seleccionVisitante = sc.nextLine();
 
-                    System.out.println("Ingrese fecha del partido:");
-                    LocalDate fecha = sc.nextDate();
-                    System.out.println("Ingrese hora del partido:");
-                    LocalTime hora = sc.nextTime();
+            Seleccion local = null;
+            Seleccion visitante = null;
+
+            for(Seleccion seleccion : grupo.getSeleccion()){
+                if(seleccion.getNombreFederacion().equalsIgnoreCase(seleccionLocal)){
+                    local = seleccion;
+                }
+                if(seleccion.getNombreFederacion().equalsIgnoreCase(seleccionVisitante)){
+                    visitante = seleccion;
+                }
+            }
+            if(local == null || visitante == null){
+                System.out.println("Una o ambas selecciones no existen en el grupo.");
+                return null;
+            }
+
+            DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm");
+
+                    System.out.println("Ingrese fecha del partido(dd/MM/yyyy):");
+                    String fechaTexto = sc.nextLine();
+                    LocalDateTime fecha = LocalDateTime.parse(fechaTexto, formatoFecha);
+                    System.out.println("Ingrese hora del partido(HH:mm):");
+                    String horaTexto = sc.nextLine();
+                    LocalTime hora = LocalTime.parse(horaTexto, formatoHora);
                     System.out.println("Ingrese duracion del partido:");
                     int duracion = sc.nextInt();
                     sc.nextLine();
                     System.out.println("Ingrese tiempo Adicional del partido:");
                     int tiempoAdicional = sc.nextInt();
-                    System.out.println("Ingrese Estadio en que se juega dicho partido:");
+                    sc.nextLine();
+                    System.out.println("Ingrese nombre del Estadio en que se juega dicho partido:");
+                    String nombreEstadio = sc.nextLine();
+                    System.out.println("Ingrese capacidad de dicho Estadio:");
+                    int capacidadEstadio = sc.nextInt();
+                    Estadio estadio = new Estadio(nombreEstadio, capacidadEstadio);
+
+                    Participacion participacion[]= new Participacion[2];
+                    participacion[0] = new Participacion(true, local, null);
+                    participacion[1] = new Participacion(true, visitante, null);
+
+                    System.out.println("Cantidad de arbitros en su partido:");
+                    int cantidadArbitros = sc.nextInt();
+                    for (int i=0; i<cantidadArbitros;i++) {
+                        System.out.println("Nombre del arbitro " + (i+1));
+                        String nombreArbitro = sc.nextLine();
+                        System.out.println("Fecha de nacimiento del arbitro " + (i+1));
+                        int fechaNacimArbitro = sc.nextInt();
+                        sc.nextLine();
+                        System.out.println("Años de experiencia del arbitro " + (i+1));
+                        int aniosExpeArbitro = sc.nextInt();
+                        System.out.println("Pais del arbitro " + (i+1));
+                        String pais = sc.nextLine();
+                        Pais paisArbitro = null;
+
+                        for (int j=0; i<paises.size();i++){
+                            if (paises.get(j).getNombre().equalsIgnoreCase(pais)){
+                                paisArbitro = paises.get(j).getNombre();
+                            }
+                    }
+                        for (CategoriaArbitro opcion : CategoriaArbitro.values()) {
+                            System.out.println("- " + opcion);
+                        }
+                        System.out.println("Categoria del arbitro " + (i+1));
+                        String categoria = sc.nextLine();
+                        CategoriaArbitro categoriaArbitro = null;
+                        for (CategoriaArbitro opcion : CategoriaArbitro.values()) {
+                            if (opcion.equals(categoria)) {
+                                categoriaArbitro = opcion;
+                            }
+                        }
+
+                        Arbitro arbitro = new Arbitro(nombreArbitro, fechaNacimArbitro, aniosExpeArbitro, paisArbitro, new Arbitraje(categoriaArbitro))
+
+                    partido = new Partido(fecha, hora, duracion,tiempoAdicional,estadio, fase,)
 
 
 
